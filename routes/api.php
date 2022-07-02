8d28f60085bd3a2d6eb84ac273020d7a66b0efc9
papermon <m.nur.fauzan5@gmail.com>
2022-07-02 23:17:52 +0700

    delete button polres

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('polres', [PolresCon::class, 'register']);
-Route::delete('polres/{id}', 'PolresCon@destroy')->name('polres.delete');
+Route::delete('polres/{id}', [PolresCon::class, 'destroy'])->name('polres.delete');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
