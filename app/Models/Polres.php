ce5fbf3b4ee1e4bba3746164d034642038faba07
anisarevina <revinarahmanisa@gmail.com>
2022-07-25 10:03:38 +0700

    database polres

--- app/Models/Polres.php
+++ app/Models/Polres.php
@@ -35,8 +35,8 @@ class Polres extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function setPasswordAttribute($value)
-    {
-        $this->attributes['password'] = bcrypt($value);
-    }
+    // public function setPasswordAttribute($value)
+    // {
+    //     $this->attributes['password'] = bcrypt($value);
+    // }
 }
