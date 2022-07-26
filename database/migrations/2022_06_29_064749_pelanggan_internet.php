115f93a8916ea2bdaa0cdf27c85d84059a867e74
anisarevina <revinarahmanisa@gmail.com>
2022-07-27 00:38:36 +0700

    fix database

--- database/migrations/2022_06_29_064749_pelanggan_internet.php
+++ database/migrations/2022_06_29_064749_pelanggan_internet.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pelangganInternet', function (Blueprint $table) {
+        Schema::create('pelangganInternets', function (Blueprint $table) {
             $table->id();
             $table->string('nomor_langganan', 30);
             $table->string('serial_number', 30);
