c523706f88bfd3abb41ea5756838fd274ee82bdf
papermon <m.nur.fauzan5@gmail.com>
2022-07-26 18:07:31 +0700

    add data personil

--- app/Models/Personil.php
+++ app/Models/Personil.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Personil extends Authenticatable
+class Personil extends Model
 {
     use HasFactory;
 
