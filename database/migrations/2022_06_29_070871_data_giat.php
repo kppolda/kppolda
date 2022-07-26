53d3a0fccbea25aad15d362ff952f01384dce0b2
anisarevina <revinarahmanisa@gmail.com>
2022-07-27 01:54:33 +0700

    migrate data giat

---
+++ database/migrations/2022_06_29_070871_data_giat.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dataGiat', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->string('tanggal')->nullable();
+            $table->string('keterangan')->nullable();
+            $table->string('file_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dataInventaris');
+    }
+};
